91ee2f1a2ca1e181af8242c0ae490f1bbc275772
YidingOu <yidingou@126.com>
2017-11-28 19:00:03 -0500

    commit changes
    commit change

--- Search/views.py
+++ Search/views.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 
 import requests
+import re
 from bs4 import BeautifulSoup
 from pyquery import PyQuery as pq
 from django.shortcuts import render
@@ -20,11 +21,56 @@ def search(request):
 	
 	# print(cao)
 	soup = BeautifulSoup(r.text)
-	line = soup.find_all("td")[6]
+	ingredient = soup.find_all("td")[6].text
+	# ingredient = [x.text for x in line]
 
-	ingredient = str(line)
+	print(ingredient)
+	
+	ingredientlst = re.split(',| and', ingredient)
+	print(ingredientlst)
+
+	drugreferencetable = {}
+
+	for i in ingredientlst:
+		#drugreferencetable.update({i:[]})
+		count = i.count(" ")
+		# print(count)
+		name = []
+		while(count>=0):
+			stri = i.replace(' ', '+')
+			url2 = "https://dailymed.nlm.nih.gov/dailymed/search.cfm?labeltype=all&query="+stri
+			print(url2)
+			r2 = requests.get(url2)
+
+			soup2 = BeautifulSoup(r2.text)
+
+			name = [x.text.strip() for x in soup2.find_all('a', href = True, class_="drug-info-link")]
+			# print(i)
+			# print(name)
+
+			if len(name) != 0:
+				break
+			else:
+				index = i.rfind(" ")
+				i = i[:index]
+				count -= 1
+
+		drugreferencetable.update({i:name})
+
+	print(drugreferencetable)
 
 
 
 
-	return HttpResponse(ingredient)
+
+
+
+
+#use ingredient to find US med
+
+
+
+
+
+
+	return HttpResponse(drugreferencetable)
