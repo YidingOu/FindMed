36f6f88568e8663595addae132c0ceaea6007ceb
YidingOu <yidingou@126.com>
2017-12-18 16:08:27 -0500

    commit
    commit

--- Search/views.py
+++ Search/views.py
@@ -48,30 +48,36 @@ def search(request):
 		#drugreferencetable.update({i:[]})
 		count = i.count(" ")
 		# print(count)
-		name = []
+		link = []
 		while(count>=0):
 			stri = i.replace(' ', '+')
 			url2 = "https://dailymed.nlm.nih.gov/dailymed/search.cfm?labeltype=all&query="+stri
-			print(url2)
+			# print(url2)
 			r2 = requests.get(url2)
 
 			soup2 = BeautifulSoup(r2.text)
 
-			name = [x.text for x in soup2.find_all('a', href = True, class_="drug-info-link")]
-			name = [x.replace("\n", '').replace("\t", '').replace("\xa0", ' ') for x in name]
+			# name = [x.text for x in soup2.find_all('a', href = True, class_="drug-info-link")]
+			link = [(x.text, x['href']) for x in soup2.find_all('a', href = True, class_="drug-info-link")]
+			# name = [x.replace("\n", '').replace("\t", '').replace("\xa0", ' ') for x in name]
+			link = [(x.replace("\n", '').replace("\t", '').replace("\xa0", ' '),y.replace("/dailymed/drugInfo.cfm?", '')) for (x,y) in link]
+
+			# link = [x.text for x in soup2.find_all('a', href = True, class_="drug-info-link")["href"]]
+
+			print(link)
 
 
 			# print(i)
 			# print(name)
 
-			if len(name) != 0:
+			if len(link) != 0:
 				break
 			else:
 				index = i.rfind(" ")
 				i = i[:index]
 				count -= 1
 
-		drugreferencetable.update({i:name})
+		drugreferencetable.update({i:link})
 
 	# print(drugreferencetable)
 
@@ -82,7 +88,7 @@ def search(request):
 	 		if k != key:
 	 			rank +=list(set(drugreferencetable[k])&set(drugreferencetable[key]))
 		
-	print(drugreferencetable)
+	# print(drugreferencetable)
 
 	if drugreferencetable == {'':[]}:
 		return error(request)
@@ -93,21 +99,48 @@ def search(request):
 	i = 'A'
 	for key, l in drugreferencetable.items():
 		if len(l) != 0:
-			result[key] =l
+			result[key] = l
 			number[key] = i
 			i = chr(ord(i)+1)
 
 	print(number)
+	print(result)
 #use ingredient to find US med
 
 
 	return render(request, 'search.html', {'drugreferencetable': result, 'number': number})
 
-def description(request, i):
+def description(request, i, j):
 
 	a = request.GET.get('request', 0)
 	print(i)
-	print(request)
+	print(j)
+	url3 = "https://dailymed.nlm.nih.gov/dailymed/drugInfo.cfm?" + j
+	r3 = requests.get(url3)
 
-	return HttpResponse("good")
+	soup3 = BeautifulSoup(r3.text)
+	des = [x.text for x in soup3.find_all('div', href = False, class_="drug-label-sections")]
+	des = [x.replace("View All SectionsClose All Sections", '').replace("Close", '\n') for x in des]
+	# print(request)
+	print(des)
+
+	return HttpResponse(des)
+
+def Buy(request, i):
+
+	stri = i.replace(" ", '+')
+	url4 = "https://primenow.amazon.com/search?k="+stri+"&p_95=&merchantId=&ref_=pn_gw_nav_sr_ALL"
+
+
+
+
+
+
+
+
+
+
+
+
+
 
